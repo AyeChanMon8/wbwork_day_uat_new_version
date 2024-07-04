57654cb30a348a208d4a2aae7adecd55d3e62180
AyeChanMon8 <ayechan.mon@digipowermm.com>
2024-07-04 12:13:23 +0630

    fix null value error when flutter version upgrade

--- lib/models/trip_expense.dart
+++ lib/models/trip_expense.dart
@@ -1,15 +1,13 @@
-// @dart=2.9
-
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 
-import 'package:winbrother_hr_app/models/company.dart';
-import 'package:winbrother_hr_app/models/employee.dart';
-import 'package:winbrother_hr_app/models/travel_expense/employee_model.dart';
-import 'package:winbrother_hr_app/models/travel_expense/pocket_line.dart';
-import 'package:winbrother_hr_app/models/travel_expense/pocket_model.dart';
-import 'package:winbrother_hr_app/models/trip_expense_line.dart';
+import '../models/company.dart';
+import '../models/employee.dart';
+import '../models/travel_expense/employee_model.dart';
+import '../models/travel_expense/pocket_line.dart';
+import '../models/travel_expense/pocket_model.dart';
+import '../models/trip_expense_line.dart';
 
 class TripExpense {
   int id;
@@ -18,83 +16,119 @@ class TripExpense {
   String state;
   String source_doc;
   double advanced_money;
-  EmployeeModel employee_id;
-  Company company_id;
+  EmployeeModel? employee_id;
+  Company? company_id;
   double total_expense;
   double diff_amount;
-  Company vendor_bill_id;
-  Company daytrip_id;
-  Company plantrip_product_id;
-  Company plantrip_waybill_id;
-  List<TripExpenseLine> trip_expense_lines;
-
-  TripExpense({
-    this.id,
-    this.date,
-    this.state,
-    this.employee_id,
-    this.company_id,
-    this.trip_expense_lines,
-    this.number, this.source_doc,
-  });
+  Company? vendor_bill_id;
+  Company? daytrip_id;
+  Company? plantrip_product_id;
+  Company? plantrip_waybill_id;
+  List<TripExpenseLine>? trip_expense_lines;
+  TripExpense(
+      {this.id = 0,
+      this.number = '',
+      this.date = '',
+      this.state = '',
+      this.source_doc = '',
+      this.advanced_money = 0.0,
+      this.employee_id,
+      this.company_id,
+      this.total_expense = 0.0,
+      this.diff_amount = 0.0,
+      this.vendor_bill_id,
+      this.daytrip_id,
+      this.plantrip_product_id,
+      this.plantrip_waybill_id,
+      this.trip_expense_lines});
 
   TripExpense copyWith({
-    int id,
-    String date,
-    String state,
-    EmployeeModel employee_id,
-    Company company_id,
-    List<PocketLine> pocket_line,
-    String number,
+    int? id,
+    String? number,
+    String? date,
+    String? state,
+    String? source_doc,
+    double? advanced_money,
+    EmployeeModel? employee_id,
+    Company? company_id,
+    double? total_expense,
+    double? diff_amount,
+    Company? vendor_bill_id,
+    Company? daytrip_id,
+    Company? plantrip_product_id,
+    Company? plantrip_waybill_id,
+    List<TripExpenseLine>? trip_expense_lines,
   }) {
     return TripExpense(
-      id: id ?? this.id,
-      date: date ?? this.date,
-      state: state ?? this.state,
-      employee_id: employee_id ?? this.employee_id,
-      company_id: company_id ?? this.company_id,
-      trip_expense_lines: trip_expense_lines ?? this.trip_expense_lines,
-      number: number ?? this.number,
-      source_doc: source_doc ?? this.source_doc
-    );
+        id: id ?? this.id,
+        number: number ?? this.number,
+        date: date ?? this.date,
+        state: state ?? this.state,
+        source_doc: source_doc ?? this.source_doc,
+        advanced_money: advanced_money ?? this.advanced_money,
+        employee_id: employee_id ?? this.employee_id,
+        company_id: company_id ?? this.company_id,
+        total_expense: total_expense ?? this.total_expense,
+        diff_amount: diff_amount ?? this.diff_amount,
+        vendor_bill_id: vendor_bill_id ?? this.vendor_bill_id,
+        daytrip_id: daytrip_id ?? this.daytrip_id,
+        plantrip_product_id: plantrip_product_id ?? this.plantrip_product_id,
+        plantrip_waybill_id: plantrip_waybill_id ?? this.plantrip_waybill_id,
+        trip_expense_lines: trip_expense_lines ?? this.trip_expense_lines);
   }
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
+      'number': number,
       'date': date,
       'state': state,
+      'source_doc': source_doc,
+      'advanced_money': advanced_money,
       'employee_id': employee_id?.toMap(),
       'company_id': company_id?.toMap(),
+      'total_expense': total_expense,
+      'diff_amount': diff_amount,
+      'vendor_bill_id': vendor_bill_id?.toMap(),
+      'daytrip_id': daytrip_id?.toMap(),
+      'plantrip_product_id': plantrip_product_id?.toMap(),
+      'plantrip_waybill_id': plantrip_waybill_id?.toMap(),
       'trip_expense_lines': trip_expense_lines?.map((x) => x?.toMap())?.toList(),
-      'number': number,
-
+      // 'name': name,
     };
   }
 
   factory TripExpense.fromMap(Map<String, dynamic> map) {
-    if (map == null) return null;
+    // if (map == null) return null;
 
     return TripExpense(
       id: map['id'],
+      number: map['number'],
       date: map['date'],
       state: map['state'],
+      source_doc: map['source_doc'],
+      advanced_money: map['advanced_money'],
       employee_id: EmployeeModel.fromMap(map['employee_id']),
       company_id: Company.fromMap(map['company_id']),
-      trip_expense_lines: List<TripExpenseLine>.from(map['trip_expense_lines']?.map((x) => TripExpenseLine.fromMap(x))),
-      number: map['number'],
-      source_doc:  map['source_doc'],
+      total_expense: map['total_expense'],
+      diff_amount: map['diff_amount'],
+      vendor_bill_id: Company.fromMap(map['vendor_bill_id']),
+      daytrip_id: Company.fromMap(map['daytrip_id']),
+      plantrip_product_id: Company.fromMap(map['plantrip_product_id']),
+      plantrip_waybill_id: Company.fromMap(map['plantrip_waybill_id']),
+      trip_expense_lines: List<TripExpenseLine>.from(
+          map['trip_expense_lines']?.map((x) => TripExpenseLine.fromMap(x))),
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory TripExpense.fromJson(String source) => TripExpense.fromMap(json.decode(source));
+  factory TripExpense.fromJson(String source) =>
+      TripExpense.fromMap(json.decode(source));
 
   @override
-  String toString() {
-    return 'TripExpense(id: $id, date: $date, state: $state, employee_id: $employee_id, company_id: $company_id, pocket_line: $trip_expense_lines, number: $number)';
-  }
+  String toString() =>
+      'TripExpense(id: $id, number: $number, date: $date, state: $state,source_doc: $source_doc, advanced_money: $advanced_money, employee_id: $employee_id, company_id: $company_id,total_expense: $total_expense, diff_amount: $diff_amount, vendor_bill_id: $vendor_bill_id, daytrip_id: $daytrip_id, plantrip_product_id: $plantrip_product_id, plantrip_waybill_id: $plantrip_waybill_id, trip_expense_lines: $trip_expense_lines)';
 
   @override
   bool operator ==(Object o) {
@@ -102,22 +136,150 @@ class TripExpense {
 
     return o is TripExpense &&
         o.id == id &&
+        o.number == number &&
         o.date == date &&
         o.state == state &&
+        o.source_doc == source_doc &&
+        o.advanced_money == advanced_money &&
         o.employee_id == employee_id &&
         o.company_id == company_id &&
-        listEquals(o.trip_expense_lines, trip_expense_lines) &&
-        o.number == number;
+        o.total_expense == total_expense &&
+        o.diff_amount == diff_amount &&
+        o.vendor_bill_id == vendor_bill_id &&
+        o.daytrip_id == daytrip_id &&
+        o.plantrip_product_id == plantrip_product_id &&
+        o.plantrip_waybill_id == plantrip_waybill_id &&
+        o.trip_expense_lines == trip_expense_lines;
   }
 
   @override
-  int get hashCode {
-    return id.hashCode ^
-    date.hashCode ^
-    state.hashCode ^
-    employee_id.hashCode ^
-    company_id.hashCode ^
-    trip_expense_lines.hashCode ^
-    number.hashCode;
-  }
+  int get hashCode =>
+      id.hashCode ^
+      number.hashCode ^
+      date.hashCode ^
+      state.hashCode ^
+      source_doc.hashCode ^
+      advanced_money.hashCode ^
+      employee_id.hashCode ^
+      company_id.hashCode ^
+      total_expense.hashCode ^
+      diff_amount.hashCode ^
+      vendor_bill_id.hashCode ^
+      daytrip_id.hashCode ^
+      plantrip_product_id.hashCode ^
+      plantrip_waybill_id.hashCode ^
+      trip_expense_lines.hashCode;
 }
+
+// class TripExpense {
+//   int id;
+//   String number;
+//   String date;
+//   String state;
+//   String source_doc;
+//   double advanced_money;
+//   EmployeeModel employee_id;
+//   Company company_id;
+//   double total_expense;
+//   double diff_amount;
+//   Company vendor_bill_id;
+//   Company daytrip_id;
+//   Company plantrip_product_id;
+//   Company plantrip_waybill_id;
+//   List<TripExpenseLine> trip_expense_lines;
+
+//   TripExpense({
+//     this.id,
+//     this.date,
+//     this.state,
+//     this.employee_id,
+//     this.company_id,
+//     this.trip_expense_lines,
+//     this.number,
+//     this.source_doc,
+//   });
+
+//   TripExpense copyWith({
+//     int id,
+//     String date,
+//     String state,
+//     EmployeeModel employee_id,
+//     Company company_id,
+//     List<PocketLine> pocket_line,
+//     String number,
+//   }) {
+//     return TripExpense(
+//         id: id ?? this.id,
+//         date: date ?? this.date,
+//         state: state ?? this.state,
+//         employee_id: employee_id ?? this.employee_id,
+//         company_id: company_id ?? this.company_id,
+//         trip_expense_lines: trip_expense_lines ?? this.trip_expense_lines,
+//         number: number ?? this.number,
+//         source_doc: source_doc ?? this.source_doc);
+//   }
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'id': id,
+//       'date': date,
+//       'state': state,
+//       'employee_id': employee_id?.toMap(),
+//       'company_id': company_id?.toMap(),
+//       'trip_expense_lines':
+//           trip_expense_lines?.map((x) => x?.toMap())?.toList(),
+//       'number': number,
+//     };
+//   }
+
+//   factory TripExpense.fromMap(Map<String, dynamic> map) {
+//     if (map == null) return null;
+
+//     return TripExpense(
+//       id: map['id'],
+//       date: map['date'],
+//       state: map['state'],
+//       employee_id: EmployeeModel.fromMap(map['employee_id']),
+//       company_id: Company.fromMap(map['company_id']),
+//       trip_expense_lines: List<TripExpenseLine>.from(
+//           map['trip_expense_lines']?.map((x) => TripExpenseLine.fromMap(x))),
+//       number: map['number'],
+//       source_doc: map['source_doc'],
+//     );
+//   }
+
+//   String toJson() => json.encode(toMap());
+
+//   factory TripExpense.fromJson(String source) =>
+//       TripExpense.fromMap(json.decode(source));
+
+//   @override
+//   String toString() {
+//     return 'TripExpense(id: $id, date: $date, state: $state, employee_id: $employee_id, company_id: $company_id, pocket_line: $trip_expense_lines, number: $number)';
+//   }
+
+//   @override
+//   bool operator ==(Object o) {
+//     if (identical(this, o)) return true;
+
+//     return o is TripExpense &&
+//         o.id == id &&
+//         o.date == date &&
+//         o.state == state &&
+//         o.employee_id == employee_id &&
+//         o.company_id == company_id &&
+//         listEquals(o.trip_expense_lines, trip_expense_lines) &&
+//         o.number == number;
+//   }
+
+//   @override
+//   int get hashCode {
+//     return id.hashCode ^
+//         date.hashCode ^
+//         state.hashCode ^
+//         employee_id.hashCode ^
+//         company_id.hashCode ^
+//         trip_expense_lines.hashCode ^
+//         number.hashCode;
+//   }
+// }
--- lib/models/trip_expense_line.dart
+++ lib/models/trip_expense_line.dart
@@ -1,15 +1,13 @@
-// @dart=2.9
-
 import 'dart:convert';
 
-import 'package:winbrother_hr_app/models/travel_expense/category_model.dart';
-import 'package:winbrother_hr_app/models/travel_expense/product_model.dart';
+import '../models/travel_expense/category_model.dart';
+import '../models/travel_expense/product_model.dart';
 
 class TripExpenseLine {
   int id;
   String date;
-  CategoryModel categ_id;
-  ProductModel product_id;
+  CategoryModel? categ_id;
+  ProductModel? product_id;
   String description;
   double qty;
   double price_unit;
@@ -18,41 +16,52 @@ class TripExpenseLine {
   String expense_title;
   String expense;
   double over_amount;
-  TripExpenseLine({
-    this.date,
-    this.categ_id,
-    this.product_id,
-    this.description,
-    this.qty,
-    this.price_unit,
-    this.price_subtotal,
-    this.attached_file,this.over_amount,this.id
-  });
+  TripExpenseLine(
+      {this.id = 0,
+      this.date = '',
+      this.categ_id,
+      this.product_id,
+      this.description = '',
+      this.qty = 0.0,
+      this.price_unit = 0.0,
+      this.price_subtotal = 0.0,
+      this.attached_file = '',
+      this.expense_title = '',
+      this.expense = '',
+      this.over_amount = 0.0});
 
   TripExpenseLine copyWith({
-    String date,
-    CategoryModel categ_id,
-    ProductModel product_id,
-    String description,
-    double qty,
-    double price_unit,
-    double price_subtotal,
-    String attached_file,
+    int? id,
+    String? date,
+    CategoryModel? categ_id,
+    ProductModel? product_id,
+    String? description,
+    double? qty,
+    double? price_unit,
+    double? price_subtotal,
+    String? attached_file,
+    String? expense_title,
+    String? expense,
+    double? over_amount,
   }) {
     return TripExpenseLine(
-      date: date ?? this.date,
-      categ_id: categ_id ?? this.categ_id,
-      product_id: product_id ?? this.product_id,
-      description: description ?? this.description,
-      qty: qty ?? this.qty,
-      price_unit: price_unit ?? this.price_unit,
-      price_subtotal: price_subtotal ?? this.price_subtotal,
-      attached_file: attached_file ?? this.attached_file,
-    );
+        id: id ?? this.id,
+        date: date ?? this.date,
+        categ_id: categ_id ?? this.categ_id,
+        product_id: product_id ?? this.product_id,
+        description: description ?? this.description,
+        qty: qty ?? this.qty,
+        price_unit: price_unit ?? this.price_unit,
+        price_subtotal: price_subtotal ?? this.price_subtotal,
+        attached_file: attached_file ?? this.attached_file,
+        expense_title: expense_title ?? this.expense_title,
+        expense: expense ?? this.expense,
+        over_amount: over_amount ?? this.over_amount);
   }
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'date': date,
       'categ_id': categ_id?.toMap(),
       'product_id': product_id?.toMap(),
@@ -61,11 +70,15 @@ class TripExpenseLine {
       'price_unit': price_unit,
       'price_subtotal': price_subtotal,
       'attached_file': attached_file,
+      'expense_title': expense_title,
+      'expense': expense,
+      'over_amount': over_amount,
+      // 'name': name,
     };
   }
 
   factory TripExpenseLine.fromMap(Map<String, dynamic> map) {
-    if (map == null) return null;
+    // if (map == null) return null;
 
     return TripExpenseLine(
       id: map['id'],
@@ -77,6 +90,8 @@ class TripExpenseLine {
       price_unit: map['price_unit'],
       price_subtotal: map['price_subtotal'],
       attached_file: map['attached_file'],
+      expense_title: map['expense_title'],
+      expense: map['expense'],
       over_amount: map['over_amount'],
     );
   }
@@ -87,15 +102,15 @@ class TripExpenseLine {
       TripExpenseLine.fromMap(json.decode(source));
 
   @override
-  String toString() {
-    return 'TripExpenseLine(date: $date, categ_id: $categ_id, product_id: $product_id, description: $description, qty: $qty, price_unit: $price_unit, price_subtotal: $price_subtotal, attached_file: $attached_file)';
-  }
+  String toString() =>
+      'TripExpenseLine(id: $id, date: $date, categ_id: $categ_id, product_id: $product_id,description: $description, qty: $qty, price_unit: $price_unit, price_subtotal: $price_subtotal,attached_file: $attached_file, expense_title: $expense_title, expense: $expense, over_amount: $over_amount)';
 
   @override
   bool operator ==(Object o) {
     if (identical(this, o)) return true;
 
     return o is TripExpenseLine &&
+        o.id == id &&
         o.date == date &&
         o.categ_id == categ_id &&
         o.product_id == product_id &&
@@ -103,18 +118,139 @@ class TripExpenseLine {
         o.qty == qty &&
         o.price_unit == price_unit &&
         o.price_subtotal == price_subtotal &&
-        o.attached_file == attached_file;
+        o.attached_file == attached_file &&
+        o.expense_title == expense_title &&
+        o.expense == expense &&
+        o.over_amount == over_amount;
   }
 
   @override
-  int get hashCode {
-    return date.hashCode ^
-    categ_id.hashCode ^
-    product_id.hashCode ^
-    description.hashCode ^
-    qty.hashCode ^
-    price_unit.hashCode ^
-    price_subtotal.hashCode ^
-    attached_file.hashCode;
-  }
+  int get hashCode =>
+      id.hashCode ^
+      date.hashCode ^
+      categ_id.hashCode ^
+      product_id.hashCode ^
+      description.hashCode ^
+      qty.hashCode ^
+      price_unit.hashCode ^
+      price_subtotal.hashCode ^
+      attached_file.hashCode ^
+      expense_title.hashCode ^
+      expense.hashCode ^
+      over_amount.hashCode;
 }
+
+// class TripExpenseLine {
+//   int id;
+//   String date;
+//   CategoryModel categ_id;
+//   ProductModel product_id;
+//   String description;
+//   double qty;
+//   double price_unit;
+//   double price_subtotal;
+//   String attached_file;
+//   String expense_title;
+//   String expense;
+//   double over_amount;
+//   TripExpenseLine(
+//       {this.date,
+//       this.categ_id,
+//       this.product_id,
+//       this.description,
+//       this.qty,
+//       this.price_unit,
+//       this.price_subtotal,
+//       this.attached_file,
+//       this.over_amount,
+//       this.id});
+
+//   TripExpenseLine copyWith({
+//     String date,
+//     CategoryModel categ_id,
+//     ProductModel product_id,
+//     String description,
+//     double qty,
+//     double price_unit,
+//     double price_subtotal,
+//     String attached_file,
+//   }) {
+//     return TripExpenseLine(
+//       date: date ?? this.date,
+//       categ_id: categ_id ?? this.categ_id,
+//       product_id: product_id ?? this.product_id,
+//       description: description ?? this.description,
+//       qty: qty ?? this.qty,
+//       price_unit: price_unit ?? this.price_unit,
+//       price_subtotal: price_subtotal ?? this.price_subtotal,
+//       attached_file: attached_file ?? this.attached_file,
+//     );
+//   }
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'date': date,
+//       'categ_id': categ_id?.toMap(),
+//       'product_id': product_id?.toMap(),
+//       'description': description,
+//       'qty': qty,
+//       'price_unit': price_unit,
+//       'price_subtotal': price_subtotal,
+//       'attached_file': attached_file,
+//     };
+//   }
+
+//   factory TripExpenseLine.fromMap(Map<String, dynamic> map) {
+//     if (map == null) return null;
+
+//     return TripExpenseLine(
+//       id: map['id'],
+//       date: map['date'],
+//       categ_id: CategoryModel.fromMap(map['categ_id']),
+//       product_id: ProductModel.fromMap(map['product_id']),
+//       description: map['description'],
+//       qty: map['qty'],
+//       price_unit: map['price_unit'],
+//       price_subtotal: map['price_subtotal'],
+//       attached_file: map['attached_file'],
+//       over_amount: map['over_amount'],
+//     );
+//   }
+
+//   String toJson() => json.encode(toMap());
+
+//   factory TripExpenseLine.fromJson(String source) =>
+//       TripExpenseLine.fromMap(json.decode(source));
+
+//   @override
+//   String toString() {
+//     return 'TripExpenseLine(date: $date, categ_id: $categ_id, product_id: $product_id, description: $description, qty: $qty, price_unit: $price_unit, price_subtotal: $price_subtotal, attached_file: $attached_file)';
+//   }
+
+//   @override
+//   bool operator ==(Object o) {
+//     if (identical(this, o)) return true;
+
+//     return o is TripExpenseLine &&
+//         o.date == date &&
+//         o.categ_id == categ_id &&
+//         o.product_id == product_id &&
+//         o.description == description &&
+//         o.qty == qty &&
+//         o.price_unit == price_unit &&
+//         o.price_subtotal == price_subtotal &&
+//         o.attached_file == attached_file;
+//   }
+
+//   @override
+//   int get hashCode {
+//     return date.hashCode ^
+//         categ_id.hashCode ^
+//         product_id.hashCode ^
+//         description.hashCode ^
+//         qty.hashCode ^
+//         price_unit.hashCode ^
+//         price_subtotal.hashCode ^
+//         attached_file.hashCode;
+//   }
+// }
